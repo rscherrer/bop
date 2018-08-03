7d5d1e6e0b5d6d96de508b6912052879d336258c
rscherrer <raphael.scherrer@evobio.eu>
2018-08-03 13:37:11 +0200

    added new functions

---
+++ R/make_mean_pephylo.R
@@ -0,0 +1,34 @@
+#' Write PEPHYLO input mean traits
+#'
+#' This function saves the MetricTraitMeans.txt input file for PEPHYLO.
+#'
+#' @param pcaOutput Either a list or a \code{prcomp} object. If list, the first element must be a \code{prcomp} object.
+#' @param outpath A string. The path to the folder where the output file is to be saved.
+#' @return Returns 1 if succeeds. Saves the output file into the specified folder.
+#' @author Raphael Scherrer
+#' @export
+
+# Function to save the MetricTraitMeans.txt input file for pephylo
+make_mean_pephylo <- function(pcaOutput, outpath) {
+
+  # Input should be a list or a prcomp object
+  # If list, the first element should be a PCA output
+  if(inherits(pcaOutput, "list")) {
+    pcaOutput <- pcaOutput[[1]]
+  }
+
+  # Now it should be a PCA output
+  if(!inherits(pcaOutput, "prcomp")) stop("pcaOutput should be a prcomp object")
+
+  # Extract the data from the PCA output
+  pcaOutput <- pcaOutput$x
+
+  # Output file path
+  outputFile <- paste(outpath, "MetricTraitMeans.txt", sep = "/")
+
+  # Write output
+  write.table(pcaOutput, outputFile, col.names = F, row.names = F)
+
+  return(1)
+
+}
