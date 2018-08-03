33a1388379b6e1d01e2416c7335ea0f29e4cbcc6
rscherrer <raphael.scherrer@evobio.eu>
2018-08-03 12:00:36 +0200

    added function

---
+++ R/remove_bad_patches.R
@@ -0,0 +1,42 @@
+#' Remove patches with NAs
+#'
+#' This function removes the patches that are not measured in all species or all sexes from a data frame
+#'
+#' @param specTab A data frame. Must have a column "patch" and a column "species".
+#' @return A subset of the input data frame with only patches that have no missing data.
+#' @author Raphael Scherrer
+#' @export
+
+# Function to remove the patches that are not measured in all species or all sexes from a data frame
+remove_bad_patches <- function(specTab) {
+
+  # Check that the input is OK
+  if(!inherits(specTab, "data.frame")) stop("specTab must be a data frame")
+  if(!all(c("patch", "species") %in% colnames(specTab))) stop("specTab should have a column 'patch' and a column 'species'")
+
+  # Record patch and species names
+  patchNames <- levels(specTab$patch)
+  speciesNames <- levels(specTab$species)
+
+  # Find out what patches are not present in ALL species and both sexes
+  isData <- sapply(patchNames, function(curr.patch) {
+
+    # What species have data for this patch?
+    curr.species <- droplevels(specTab$species[specTab$patch == curr.patch])
+
+    # If males and females of each species have this patch
+    # The length of curr.species should be twice that of the number of species
+    answer <- length(curr.species) == 2 * length(speciesNames)
+
+  })
+
+  # What are the patches to keep?
+  goodPatches <- names(isData)[isData]
+
+  specTab <- droplevels(specTab[specTab$patch %in% goodPatches,])
+
+  if(!all(levels(specTab$patch) == goodPatches)) stop("specTab should contain only patches without missing data")
+
+  return(specTab)
+
+}
