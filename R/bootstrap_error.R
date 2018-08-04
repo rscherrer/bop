bb485ed53613c0755abdb48659550d17b954f6b2
rscherrer <raphael.scherrer@evobio.eu>
2018-08-04 15:43:15 +0200

    printing to prompt is optional

--- R/bootstrap_error.R
+++ R/bootstrap_error.R
@@ -4,12 +4,13 @@
 #'
 #' @param x A vector of deviations from the mean (or any numbers).
 #' @param N An integer. The number of samplings to perform.
+#' @param printit Logical. Whether to print the iteration to the prompt.
 #' @return The standard deviation of the re-calculated means of all bootstrapped samples.
 #' @author Raphael Scherrer
 #' @export
 
 # Function to compute the error (standard deviation) on the mean
-bootstrap_error <- function(x, N = 999) {
+bootstrap_error <- function(x, N = 999, printit = F) {
 
   # Length of the vector to bootstrap
   n <- length(x)
@@ -17,7 +18,7 @@ bootstrap_error <- function(x, N = 999) {
   # For each permutation
   bootMeans <- sapply(seq_len(N), function(i) {
 
-    print(i)
+    if(printit) print(i)
 
     # Sample a new random vector with replacement
     y <- sample(x, n, replace = T)
