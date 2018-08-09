41dfe2a15a8f68c1a27b1d2f9e4d6dd3abc150c8
rscherrer <raphael.scherrer@evobio.eu>
2018-08-09 15:50:50 +0200

    added tree checking

--- R/check_pephylo.R
+++ R/check_pephylo.R
@@ -2,16 +2,17 @@
 #'
 #' This function displays the files in the input folder for PEPHYLO, to check they are alright before running the program.
 #'
-#' @param filename Either a string with the name of the file to be viewed, either of "MetricTraitMeans", "MetricTraitStds", "MetricSpNames", "SpExt" or "MetricRates", with or without the ".txt" extension. Or, an integer representing the position of the file to view in the list described in the previous sentence.
+#' @param filename Either a string with the name of the file to be viewed, either of "MetricTraitMeans", "MetricTraitStds", "MetricSpNames", "SpExt", "MetricRates" or "Trees", with or without the ".txt" extension. Or, an integer representing the position of the file to view in the list described in the previous sentence.
 #' @param outpath A string. The path to the folder where the output file is to be saved.
+#' @param plotit Logical. Whether to plot the first phylogeny provided (applicable only if filename is Trees.txt or 6).
 #' @return Displays the content of the file using \code{View}.
 #' @author Raphael Scherrer
 #' @export
 
 # Function to check that the output is correctly made
-check_pephylo <- function(filename, outpath) {
+check_pephylo <- function(filename, outpath, plotit = T) {
 
-  filenames <- c("MetricTraitMeans", "MetricTraitStds", "MetricSpNames", "SpExt", "MetricRates")
+  filenames <- c("MetricTraitMeans", "MetricTraitStds", "MetricSpNames", "SpExt", "MetricRates", "Trees")
 
   if(inherits(filename, "character")) {
     if(length(grep("txt$", filename) == 0)) {
@@ -21,7 +22,26 @@ check_pephylo <- function(filename, outpath) {
     filename <- paste(filenames[filename], "txt", sep = ".")
   }
 
-  d <- read.table(paste(outpath, filename, sep = "/"))
-  View(d)
+  if(length(grep("Trees", filename)) == 0) {
+    d <- read.table(paste(outpath, filename, sep = "/"))
+    View(d)
+  } else {
+
+    if(filename %in% list.files(outpath)) {
+      message("Yes, Trees.txt is in the input folder")
+      library(ape)
+      if(plotit) {
+        tree <- read.tree(paste(outpath, filename, sep = "/"))
+        if(inherits(trees, "multiPhylo")) tree <- tree[[1]]
+        plot(tree)
+        axisPhylo()
+      }
+    } else {
+      message("Nope, no Trees.txt here...")
+    }
+
+
+  }
+
 
 }
